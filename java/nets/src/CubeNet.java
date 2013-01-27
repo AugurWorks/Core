7a6c3afb734a202c4e286bf5aecd925fa5d46806
Stephen Freiberg <safreiberg@gmail.com>
2013-01-27 18:05:12 -0500

    File io added to training, but training no longer working properly. Refactor appears to have broken some training code, and the convergence is now sensitive to initial conditions of network.

--- java/nets/src/CubeNet.java
+++ java/nets/src/CubeNet.java
@@ -5,6 +5,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class CubeNet {
 	private Input[][] inputs;
@@ -12,7 +13,7 @@ public class CubeNet {
 	private int x;
 	private int y;
 	private int z;
-	public static boolean verbose = true;
+	public static boolean verbose = false;
 
 	public CubeNet() {
 		this.x = 10;
@@ -32,6 +33,14 @@ public class CubeNet {
 		for (int i = 0; i < x; i++) {
 			for (int j = 0; j < y; j++) {
 				this.inputs[i][j].setValue(nums[i][j]);
+			}
+		}
+	}
+	
+	public void setInputs(double[][][] nums, int depth) {
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				this.inputs[i][j].setValue(nums[i][j][depth]);
 			}
 		}
 	}
@@ -64,7 +73,7 @@ public class CubeNet {
 						for (int ii = 0; ii < x; ii++) {
 							for (int jj = 0; jj < y; jj++) {
 								this.neurons[i][j][k].addInput(
-										this.neurons[ii][jj][k - 1], 0);
+										this.neurons[ii][jj][k - 1], Math.random());
 							}
 						}
 					}
@@ -121,222 +130,218 @@ public class CubeNet {
 	}
 
 	public void train(double r, double samples[][][],
-			double[][][] desiredOutput, double perfLimit) {
+			double[][][] desiredOutput, double perfLimit, int innerIter,
+			int outerIter) {
 		assert (samples.length == x);
 		assert (samples[0].length == y);
 		assert (samples[0][0].length == desiredOutput.length);
 		int numSamples = samples[0][0].length;
 		// 1) pick a rate parameter r
 		// 2) until perf is satisfactory
-		int kill = 0;
-		int lcv = 0;
 		double min = Double.NEGATIVE_INFINITY;
-		while (true) {
-			kill++;
-			if (kill > 1000) {
-				kill = 0;
-				lcv++;
-				init();
-			}
+		for (int outCount = 0; outCount < outerIter; outCount++) {
 			double[][] actual;
 			double[][] desired = new double[x][y];
 			double[][] differenceSquared = new double[x][y];
 			double[][][] weightDeltas = new double[x][y][z];
 			// for each sample input
 			for (int sampleNumber = 0; sampleNumber < numSamples; sampleNumber++) {
-				// System.out.println("Sample number: " + sampleNumber);
-				double[][] samp = new double[x][y];
-				/*
-				 * System.out.println("x: "+x); System.out.println("y: "+y);
-				 */
-				for (int i = 0; i < x; i++) {
-					for (int j = 0; j < y; j++) {
-						samp[i][j] = samples[i][j][sampleNumber];
-					}
-				}
-				if (verbose) {
-					System.out.println("\nSample array:");
+				for (int count = 0; count < innerIter; count++) {
+					// System.out.println("Sample number: " + sampleNumber);
+					double[][] samp = new double[x][y];
+					/*
+					 * System.out.println("x: "+x); System.out.println("y: "+y);
+					 */
 					for (int i = 0; i < x; i++) {
 						for (int j = 0; j < y; j++) {
-							/*
-							 * System.out.println("i: "+i);
-							 * System.out.println("j: "+j);
-							 * System.out.println("sn: "+sampleNumber);
-							 * System.out.println("samp: " + samp[i][j]);
-							 * System.out.
-							 * println("s: "+samples[i][j][sampleNumber]);
-							 */
-							System.out.print("(" + i + "," + j + "): "
-									+ samp[i][j] + "   ");
+							samp[i][j] = samples[i][j][sampleNumber];
 						}
-						System.out.print("\n");
-					}
-					System.out.print("\n");
-				}
-
-				// samp is now the sample input array
-				// 3) compute the resulting output
-				setInputs(samp);
-				actual = getOutput();
-				if (verbose) {
-					System.out.println("Inputs set. Output[0][0] is: "
-							+ actual[0][0]);
-				}
-				for (int i = 0; i < x; i++) {
-					for (int j = 0; j < y; j++) {
-						desired[i][j] = desiredOutput[i][j][sampleNumber];
-					}
-				}
-				if (verbose) {
-					System.out.println("\nDesired array:");
-					for (int i = 0; i < x; i++) {
-						for (int j = 0; j < y; j++) {
-							System.out.print("(" + i + "," + j + "): "
-									+ desired[i][j] + "   ");
-						}
-						System.out.print("\n");
-					}
-					System.out.print("\n");
-				}
-
-				for (int i = 0; i < x; i++) {
-					for (int j = 0; j < y; j++) {
-						if (verbose) {
-							System.out.print("(" + i + "," + j + "): D:"
-									+ desired[i][j] + ", A: " + actual[i][j]
-									+ "   ");
-						}
-						differenceSquared[i][j] += (desired[i][j] - actual[i][j])
-								* (desired[i][j] - actual[i][j]);
 					}
 					if (verbose) {
-						System.out.print("\n");
-					}
-				}
-
-				if (verbose) {
-					System.out.println("\nDiff^2 array:");
-					for (int i = 0; i < x; i++) {
-						for (int j = 0; j < y; j++) {
-							System.out.print("(" + i + "," + j + "): "
-									+ differenceSquared[i][j] + "   ");
-						}
-						System.out.print("\n");
-					}
-					System.out.print("\n");
-				}
-
-				double[][][] b = new double[x][y][z];
-				// 4) compute B for all nodes in the output layer using
-				// Bz = dz - oz
-				for (int i = 0; i < x; i++) {
-					for (int j = 0; j < y; j++) {
-						b[i][j][z - 1] = desiredOutput[i][j][sampleNumber]
-								- actual[i][j];
-					}
-				}
-				// 5) compute B for all other nodes using
-				// Bj = sum k ( wj->k ok( 1 - ok ) Bk
-				// from right to left:
-
-				for (int i = 0; i < x; i++) {
-					for (int j = 0; j < y; j++) {
-						for (int k = z - 2; k > 0; k--) {
-							for (int ii = 0; ii < x; ii++) {
-								for (int jj = 0; jj < y; jj++) {
-									double wjk = neurons[i][j][k + 1]
-											.getWeight(neurons[ii][jj][k]);
-									double ok = neurons[ii][jj][k + 1]
-											.getLastOutput();
-									double bk = b[ii][jj][k + 1];
-									b[i][j][k] += wjk * ok * (1 - ok) * bk;
-								}
-							}
-						}
-					}
-				}
-
-				// 6) compute weight changes for all weights using
-				// dWi->j = r oi oj ( 1 - oj ) Bj
-				double[][][] dw = new double[x][y][z];
-				for (int i = 0; i < x; i++) {
-					for (int j = 0; j < y; j++) {
-						for (int k = 0; k < z - 1; k++) {
-							double oi = neurons[i][j][k].getLastOutput();
-							double oj = neurons[i][j][k + 1].getLastOutput();
-							double bj = b[i][j][k + 1];
-							dw[i][j][k] = r * oi * oj * (1 - oj) * bj;
-							// System.out.print("dw: ");
-							// System.out.print(dw[i][j][k] + " ");
-						}
-					}
-				}
-				// put the dw into the weight deltas
-				for (int i = 0; i < x; i++) {
-					for (int j = 0; j < y; j++) {
-						for (int k = 0; k < z; k++) {
-							weightDeltas[i][j][k] += dw[i][j][k];
-						}
-					}
-				}
-				if (verbose) {
-					System.out.println("\nWeight Deltas arrays:");
-					for (int k = 0; k < z; k++) {
-						System.out.println("K: " + k);
+						System.out.println("\nSample array:");
 						for (int i = 0; i < x; i++) {
 							for (int j = 0; j < y; j++) {
+								/*
+								 * System.out.println("i: "+i);
+								 * System.out.println("j: "+j);
+								 * System.out.println("sn: "+sampleNumber);
+								 * System.out.println("samp: " + samp[i][j]);
+								 * System.out.
+								 * println("s: "+samples[i][j][sampleNumber]);
+								 */
 								System.out.print("(" + i + "," + j + "): "
-										+ weightDeltas[i][j][k] + "   ");
+										+ samp[i][j] + "   ");
 							}
 							System.out.print("\n");
 						}
 						System.out.print("\n");
 					}
-				}
-			}
-			// Check the break condition
-			double ds = 0;
-			for (int i = 0; i < x; i++) {
-				for (int j = 0; j < y; j++) {
-					ds += differenceSquared[i][j];
-				}
-			}
-			ds *= -1;
-			if (ds > min) {
-				min = ds;
-				// System.out.println(ds);
-			}
-			// if (ds > perfLimit) {
-			// break;
-			// }
-			// 7) add up the weight changes for all the sample inputs
-			// and change the weights
-			// new weight deltas are in weightDeltas
-			if (verbose) {
-				for (int k = 1; k < z; k++) {
-					for (int ii = 0; ii < x; ii++) {
-						for (int jj = 0; jj < x; jj++) {
-							System.out.println("(" + ii + "," + jj + "," + k
-									+ ") " + weightDeltas[ii][jj][k - 1]);
+
+					// samp is now the sample input array
+					// 3) compute the resulting output
+					setInputs(samp);
+					actual = getOutput();
+					if (verbose) {
+						System.out.println("Inputs set. Output[0][0] is: "
+								+ actual[0][0]);
+					}
+					for (int i = 0; i < x; i++) {
+						for (int j = 0; j < y; j++) {
+							desired[i][j] = desiredOutput[i][j][sampleNumber];
 						}
 					}
-				}
-			}
+					if (verbose) {
+						System.out.println("\nDesired array:");
+						for (int i = 0; i < x; i++) {
+							for (int j = 0; j < y; j++) {
+								System.out.print("(" + i + "," + j + "): "
+										+ desired[i][j] + "   ");
+							}
+							System.out.print("\n");
+						}
+						System.out.print("\n");
+					}
 
-			for (int i = 0; i < x; i++) {
-				for (int j = 0; j < y; j++) {
-					for (int k = 1; k < z; k++) {
-						for (int ii = 0; ii < x; ii++) {
-							for (int jj = 0; jj < x; jj++) {
-								this.neurons[i][j][k].changeWeight(
-										this.neurons[ii][jj][k - 1],
-										weightDeltas[ii][jj][k - 1]);
+					for (int i = 0; i < x; i++) {
+						for (int j = 0; j < y; j++) {
+							if (verbose) {
+								System.out.print("(" + i + "," + j + "): D:"
+										+ desired[i][j] + ", A: "
+										+ actual[i][j] + "   ");
+							}
+							differenceSquared[i][j] += (desired[i][j] - actual[i][j])
+									* (desired[i][j] - actual[i][j]);
+						}
+						if (verbose) {
+							System.out.print("\n");
+						}
+					}
+
+					if (verbose) {
+						System.out.println("\nDiff^2 array:");
+						for (int i = 0; i < x; i++) {
+							for (int j = 0; j < y; j++) {
+								System.out.print("(" + i + "," + j + "): "
+										+ differenceSquared[i][j] + "   ");
+							}
+							System.out.print("\n");
+						}
+						System.out.print("\n");
+					}
+
+					double[][][] b = new double[x][y][z];
+					// 4) compute B for all nodes in the output layer using
+					// Bz = dz - oz
+					for (int i = 0; i < x; i++) {
+						for (int j = 0; j < y; j++) {
+							b[i][j][z - 1] = desiredOutput[i][j][sampleNumber]
+									- actual[i][j];
+						}
+					}
+					// 5) compute B for all other nodes using
+					// Bj = sum k ( wj->k ok( 1 - ok ) Bk
+					// from right to left:
+
+					for (int i = 0; i < x; i++) {
+						for (int j = 0; j < y; j++) {
+							for (int k = z - 2; k > 0; k--) {
+								for (int ii = 0; ii < x; ii++) {
+									for (int jj = 0; jj < y; jj++) {
+										double wjk = neurons[i][j][k + 1]
+												.getWeight(neurons[ii][jj][k]);
+										double ok = neurons[ii][jj][k + 1]
+												.getLastOutput();
+										double bk = b[ii][jj][k + 1];
+										b[i][j][k] += wjk * ok * (1 - ok) * bk;
+									}
+								}
+							}
+						}
+					}
+
+					// 6) compute weight changes for all weights using
+					// dWi->j = r oi oj ( 1 - oj ) Bj
+					double[][][] dw = new double[x][y][z];
+					for (int i = 0; i < x; i++) {
+						for (int j = 0; j < y; j++) {
+							for (int k = 0; k < z - 1; k++) {
+								double oi = neurons[i][j][k].getLastOutput();
+								double oj = neurons[i][j][k + 1]
+										.getLastOutput();
+								double bj = b[i][j][k + 1];
+								dw[i][j][k] = r * oi * oj * (1 - oj) * bj;
+								// System.out.print("dw: ");
+								// System.out.print(dw[i][j][k] + " ");
+							}
+						}
+					}
+					// put the dw into the weight deltas
+					for (int i = 0; i < x; i++) {
+						for (int j = 0; j < y; j++) {
+							for (int k = 0; k < z; k++) {
+								weightDeltas[i][j][k] += dw[i][j][k];
+							}
+						}
+					}
+					if (verbose) {
+						System.out.println("\nWeight Deltas arrays:");
+						for (int k = 0; k < z; k++) {
+							System.out.println("K: " + k);
+							for (int i = 0; i < x; i++) {
+								for (int j = 0; j < y; j++) {
+									System.out.print("(" + i + "," + j + "): "
+											+ weightDeltas[i][j][k] + "   ");
+								}
+								System.out.print("\n");
+							}
+							System.out.print("\n");
+						}
+					}
+					// Check the break condition
+					double ds = 0;
+					for (int i = 0; i < x; i++) {
+						for (int j = 0; j < y; j++) {
+							ds += differenceSquared[i][j];
+						}
+					}
+					ds *= -1;
+					if (ds > min) {
+						min = ds;
+						// System.out.println(ds);
+					}
+					// if (ds > perfLimit) {
+					// break;
+					// }
+					// 7) add up the weight changes for all the sample inputs
+					// and change the weights
+					// new weight deltas are in weightDeltas
+					if (verbose) {
+						for (int k = 1; k < z; k++) {
+							for (int ii = 0; ii < x; ii++) {
+								for (int jj = 0; jj < x; jj++) {
+									System.out.println("(" + ii + "," + jj
+											+ "," + k + ") "
+											+ weightDeltas[ii][jj][k - 1]);
+								}
+							}
+						}
+					}
+
+					for (int i = 0; i < x; i++) {
+						for (int j = 0; j < y; j++) {
+							for (int k = 1; k < z; k++) {
+								for (int ii = 0; ii < x; ii++) {
+									for (int jj = 0; jj < x; jj++) {
+										this.neurons[i][j][k].changeWeight(
+												this.neurons[ii][jj][k - 1],
+												weightDeltas[ii][jj][k - 1]);
+									}
+								}
 							}
 						}
 					}
 				}
 			}
-			break;
 		}
 	}
 
@@ -366,6 +371,101 @@ public class CubeNet {
 		if (verbose) {
 			System.out.println("Valid file? " + valid);
 		}
+		// Now we need to pull information out of the augtrain file.
+		Charset charset = Charset.forName("US-ASCII");
+		Path file = Paths.get(fileName);
+		String line = null;
+		int lineNumber = 1;
+		String[] lineSplit;
+		int side = 0;
+		int depth = 0;
+		int rowIter = 0;
+		int fileIter = 0;
+		ArrayList<Double[][]> inputSets = new ArrayList<Double[][]>();
+		ArrayList<Double> targets = new ArrayList<Double>();
+		try (BufferedReader reader = Files.newBufferedReader(file, charset)) {
+			while ((line = reader.readLine()) != null) {
+				try {
+					lineSplit = line.split(" ");
+					switch (lineNumber) {
+					case 1:
+						String[] size = lineSplit[1].split(",");
+						side = Integer.valueOf(size[0]);
+						depth = Integer.valueOf(size[2]);
+						break;
+					case 2:
+						size = lineSplit[1].split(",");
+						rowIter = Integer.valueOf(size[0]);
+						fileIter = Integer.valueOf(size[1]);
+						break;
+					case 3:
+						break;
+					default:
+						// expected
+						double target = Double.valueOf(lineSplit[0]);
+						targets.add(target);
+						// inputs
+						Double[][] input = new Double[side][side];
+						size = lineSplit[1].split(",");
+						for (int i = 0; i < side; i++) {
+							for (int j = 0; j < side; j++) {
+								input[i][j] = Double
+										.valueOf(size[i * side + j]);
+							}
+						}
+						inputSets.add(input);
+						break;
+					}
+					lineNumber++;
+				} catch (Exception e) {
+					if (verbose)
+						System.err.println("Training failed at line: "
+								+ lineNumber);
+					System.exit(1);
+				}
+			}
+		} catch (IOException x) {
+			if (verbose)
+				System.err.format("IOException: %s%n", x);
+			System.exit(1);
+		}
+		// Now put the training data in an [][][]
+		double[][][] training = new double[side][side][inputSets.size()];
+		for (int k = 0; k < inputSets.size(); k++) {
+			for (int i = 0; i < side; i++) {
+				for (int j = 0; j < side; j++) {
+					training[i][j][k] = inputSets.get(k)[i][j];
+				}
+			}
+		}
+		double[][][] targ = new double[side][side][inputSets.size()];
+		for (int k = 0; k < inputSets.size(); k++) {
+			for (int i = 0; i < side; i++) {
+				for (int j = 0; j < side; j++) {
+					if (i == 0 && j == 0) {
+						targ[i][j][k] = targets.get(k);
+					} else {
+						targ[i][j][k] = 0.5;
+					}
+				}
+			}
+		}
+		// Create the net
+		CubeNet c = new CubeNet(side, side, depth);
+		// set weights as random nonzeros
+		// TODO
+		// run the training program
+		c.train(0.1, training, targ, -1, rowIter, fileIter);
+		// test her out
+		c.setInputs(training,0);
+		System.out.println(c.getOutput()[0][0]);
+		c.setInputs(training,1);
+		System.out.println(c.getOutput()[0][0]);
+		c.setInputs(training,2);
+		System.out.println(c.getOutput()[0][0]);
+		c.setInputs(training,3);
+		System.out.println(c.getOutput()[0][0]);
+		System.exit(0);
 	}
 
 	public static boolean validateAUGt(String fileName) {
@@ -542,16 +642,16 @@ public class CubeNet {
 		int intermediateRounds = 1000;
 		for (int i = 0; i < trainingRounds; i++) {
 			for (int j = 0; j < intermediateRounds; j++) {
-				c.train(0.1, samples00, des00, -1);
+//				c.train(0.1, samples00, des00, -1);
 			}
 			for (int j = 0; j < intermediateRounds; j++) {
-				c.train(0.1, samples01, des01, -1);
+//				c.train(0.1, samples01, des01, -1);
 			}
 			for (int j = 0; j < intermediateRounds; j++) {
-				c.train(0.1, samples10, des10, -1);
+//				c.train(0.1, samples10, des10, -1);
 			}
 			for (int j = 0; j < intermediateRounds; j++) {
-				c.train(0.1, samples11, des11, -1);
+//				c.train(0.1, samples11, des11, -1);
 			}
 		}
 
@@ -663,7 +763,7 @@ public class CubeNet {
 
 		int trainingRounds = 1000;
 		for (int i = 0; i < trainingRounds; i++) {
-			c.train(0.1, samples, desO, -1);
+//			c.train(0.1, samples, desO, -1);
 			output = c.getOutput()[0][0];
 			double diff = (output - desO[0][0][0]);
 			if (diff < mindiff) {
@@ -762,7 +862,7 @@ public class CubeNet {
 			}
 		}
 
-		c.train(rate, trainingData, expected, perfLimit);
+//		c.train(rate, trainingData, expected, perfLimit);
 		System.out.println("Trained:");
 
 		// and some test data
