e39c4c49bfbb859a626a619c6d4bf475f6a0fdc2
Stephen Freiberg <safreiberg@gmail.com>
2013-02-03 14:47:26 -0500

    Created the TwitterRead project. This currently shows a sample of the twitter "garden hose" that is rate limited. Note that the consumer keys are secret.
    Also refactored the directory structure so that multiple projects
    can reside in the "Core" directory.

---
+++ TwitterRead/src/TwitterReader.java
@@ -0,0 +1,67 @@
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.auth.OAuthAuthorization;
+import twitter4j.conf.ConfigurationBuilder;
+
+// Requires:
+// Twitter4j library
+// The Apache HttpClient library simplifies handling HTTP requests. 
+// To use this library download the binaries with dependencies from 
+// http://hc.apache.org/ and add then to your project class path.
+public class TwitterReader {
+	private static final String ACCESS_TOKEN = "1135747976-OyBreDVCujPbbfPLLUnV19kJ21qVTwvyxbKMnyn";
+	private static final String ACCESS_TOKEN_SECRET = "yWa2a6HOCdWRd5K7Svb6KXb9G7MCd65xIUvDT8rCYo";
+	private static final String CONSUMER_KEY = "iLQm2VraHR2MLe2mwS2mvg";
+	private static final String CONSUMER_SECRET = "BNx6ij82BtzMVoEEvk6Y4xBDRjdAV8arWysSU7YmGA";
+
+	public static void main(String[] args) {
+		System.out.println("Starting connect...");
+
+		ConfigurationBuilder builder = new ConfigurationBuilder();
+		builder.setOAuthAccessToken(ACCESS_TOKEN);
+		builder.setOAuthAccessTokenSecret(ACCESS_TOKEN_SECRET);
+		builder.setOAuthConsumerKey(CONSUMER_KEY);
+		builder.setOAuthConsumerSecret(CONSUMER_SECRET);
+
+		StatusListener listener = new StatusListener() {
+			@Override
+			public void onStatus(Status status) {
+				System.out.println(status.getUser().getName() + " : "
+						+ status.getText());
+			}
+
+			@Override
+			public void onDeletionNotice(
+					StatusDeletionNotice statusDeletionNotice) {
+			}
+
+			@Override
+			public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+			}
+
+			@Override
+			public void onException(Exception ex) {
+				ex.printStackTrace();
+			}
+
+			@Override
+			public void onScrubGeo(long arg0, long arg1) {
+			}
+
+			@Override
+			public void onStallWarning(StallWarning arg0) {
+			}
+		};
+		// sample() method internally creates a thread which manipulates
+		// TwitterStream and calls these adequate listener methods continuously.
+		OAuthAuthorization auth = new OAuthAuthorization(builder.build());
+		TwitterStream twitterStream = new TwitterStreamFactory()
+				.getInstance(auth);
+		twitterStream.addListener(listener);
+		twitterStream.sample();
+	}
+}
