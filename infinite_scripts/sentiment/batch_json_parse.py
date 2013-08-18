6d2cf102bcc8c6594a8f6148a6814163fc4f7f30
TheConnMan <bcconn2112@gmail.com>
2013-08-18 14:29:54 -0700

    Merge

--- infinite_scripts/sentiment/batch_json_parse.py
+++ infinite_scripts/sentiment/batch_json_parse.py
@@ -2,8 +2,34 @@
 
 import json
 import sys
+import os
 
-fp = open('response.txt','r')
+# Keyword file
+keyword_file = sys.argv[1]
+
+# Input file will be the responses from the infinite server
+input_file = sys.argv[2]
+
+# Output file will be where the sentiment is aggregated
+output_file = sys.argv[3]
+
+full_output_file = sys.argv[4]
+
+if os.path.isfile(keyword_file) == False:
+	print "Keyword file does not exist. Exiting now."
+	sys.exit(1)
+if os.path.isfile(input_file) == False:
+	print "Input file from infinite does not exist. Exiting now."
+	sys.exit(1)
+if os.path.isfile(output_file) == True:
+	print "Output file already exists. Exiting now to avoid data loss."
+	sys.exit(1)
+if os.path.isfile(full_output_file) == True:
+	print "Full output file already exists. Exiting now to avoid data loss."
+	sys.exit(1)
+
+
+fp = open(input_file,'r')
 
 # Will contain key,value pairs:
 #	disambiguated_name : [(start,end,keyword,sentiment,significance),( ... ), ... ]
@@ -28,6 +54,7 @@ for line in fp:
 			for j in range(len(data["data"][i]["entities"])):
 				try:
 					name = str(data["data"][i]["entities"][j]["disambiguated_name"])
+					name = name.lower()
 					sentiment = float(data["data"][i]["entities"][j]["sentiment"])
 					significance = float(data["data"][i]["entities"][j]["significance"])
 					if name in info:
@@ -38,6 +65,7 @@ for line in fp:
 				except: 
 					continue
 		for disambiguated_name in info:
+			disambiguated_name = disambiguated_name.lower()
 			(sentiment,significance) = info[disambiguated_name]
 			if disambiguated_name not in sentiments:
 				sentiments[disambiguated_name] = []
@@ -45,7 +73,7 @@ for line in fp:
 		
 	meta_string = not meta_string
 
-fp2 = open('ftext_words.txt', 'r')
+fp2 = open(keyword_file, 'r')
 fp2.readline()
 fp2.readline()
 target_words = []
@@ -53,18 +81,27 @@ for line in fp2:
 	line = line.rstrip()
 	target_words.append(line)
 
-print "target words: " + str(target_words)
+
 date_to_total_score = dict()
+date_to_sentiment = dict()
 for i in target_words:
 	for j in sentiments:
-		if i in j:
-			list_of_scores = sentiments[j]
-			for entry in list_of_scores:
-				if entry[0] not in date_to_total_score:
-					date_to_total_score[entry[0]] = 0
-				date_to_total_score[entry[0]] = date_to_total_score[entry[0]] + (float(entry[3])*float(entry[4]))
+		list_of_scores = sentiments[j]
+		for entry in list_of_scores:
+			if entry[0] not in date_to_total_score:
+				date_to_total_score[entry[0]] = 0
+			date_to_total_score[entry[0]] = date_to_total_score[entry[0]] + (float(entry[3])*float(entry[4]))
+			if (entry[0],entry[2].lower()) not in date_to_sentiment:
+				date_to_sentiment[(entry[0],entry[2].lower())] = 0
+			date_to_sentiment[(entry[0],entry[2].lower())] = date_to_sentiment[(entry[0],entry[2].lower())] + (float(entry[3])*float(entry[4]))
 
+fp3 = open(output_file, "w")
 for i in sorted(date_to_total_score, reverse=True):
-	print str(i) + "," + str(date_to_total_score[i])
-#for i in sentiments:
-#	print str(i) + " : " + str(sentiments[i])
+	fp3.write(str(i) + "," + str(date_to_total_score[i]))
+
+fp3 = open(full_output_file, "w")
+for i in sorted(date_to_sentiment, reverse=True):
+	fp3.write(str(i) + "," + str(date_to_sentiment[i]) + "\n")
+
+
+sys.exit(0)
