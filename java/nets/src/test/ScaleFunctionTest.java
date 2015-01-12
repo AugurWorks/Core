a366120c9308bf95f7b19ff0514dbcbf1b6d1c73
Stephen Freiberg <safreiberg@gmail.com>
2015-01-11 20:17:27 -0800

    ScaleFunction test

---
+++ java/nets/src/test/ScaleFunctionTest.java
@@ -0,0 +1,55 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import alfred.ScaleFunction;
+
+public class ScaleFunctionTest {
+
+    private static final double PRECISION = 0.001;
+
+    @Test
+    public void test1() {
+        ScaleFunction sf = new ScaleFunction(0, 1, 0, 1);
+        assertEquals(1, sf.normalize(1.0), PRECISION);
+        assertEquals(0, sf.normalize(0.0), PRECISION);
+        assertEquals(0.3, sf.normalize(0.3), PRECISION);
+        assertEquals(0.7, sf.normalize(0.7), PRECISION);
+
+        assertEquals(1, sf.denormalize(1.0), PRECISION);
+        assertEquals(0, sf.denormalize(0.0), PRECISION);
+        assertEquals(0.3, sf.denormalize(0.3), PRECISION);
+        assertEquals(0.7, sf.denormalize(0.7), PRECISION);
+    }
+
+    @Test
+    public void test2() {
+        ScaleFunction sf = new ScaleFunction(0, 2, 0, 1);
+        assertEquals(0.5, sf.normalize(1.0), PRECISION);
+        assertEquals(1.0, sf.normalize(2.0), PRECISION);
+        assertEquals(0.75, sf.normalize(1.5), PRECISION);
+        assertEquals(0.25, sf.normalize(0.5), PRECISION);
+
+        assertEquals(1.0, sf.denormalize(0.5), PRECISION);
+        assertEquals(2.0, sf.denormalize(1.0), PRECISION);
+        assertEquals(1.5, sf.denormalize(0.75), PRECISION);
+        assertEquals(0.5, sf.denormalize(0.25), PRECISION);
+    }
+
+    @Test
+    public void test3() {
+        ScaleFunction sf = new ScaleFunction(-1, 1, 0, 1);
+        assertEquals(0.5, sf.normalize(0.0), PRECISION);
+        assertEquals(1.0, sf.normalize(1.0), PRECISION);
+        assertEquals(0, sf.normalize(-1.0), PRECISION);
+        assertEquals(0.75, sf.normalize(0.5), PRECISION);
+
+        assertEquals(0.0, sf.denormalize(0.5), PRECISION);
+        assertEquals(1.0, sf.denormalize(1.0), PRECISION);
+        assertEquals(-1.0, sf.denormalize(0), PRECISION);
+        assertEquals(0.5, sf.denormalize(0.75), PRECISION);
+    }
+
+}
